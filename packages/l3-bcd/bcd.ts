3fdbaa991120d20795e1eb0480488b873d5f72a5
brattik <bulki86@gmail.com>
2026-04-03 10:49:30 +0300

    домашка 4 bcd с упаковкой в один байт

--- packages/l3-bcd/bcd.ts
+++ packages/l3-bcd/bcd.ts
@@ -1,5 +1,11 @@
+
+const FIRST_MASK = 0b00001111;
+const SECOND_MASK = 0b11110000;
+
 export class BCD {
   private nums: Uint8Array;
+  private digits: number = 0;
+
 
   constructor(num: number | bigint) {
     this.nums = this.split(num);
@@ -14,41 +20,74 @@ export class BCD {
   }
 
   toString(): string {
-    return Array.from(this.nums).map(String).reverse().join("")
+    return this.nums.reduce((str, i, index, origin) => {
+      const [first, second] = this.unpack(i);
+      if (this.isLast(origin, index) && this.isOdd(this.digits)) {
+        return `${first}${str}`;
+      }
+
+      return `${second}${first}${str}`;
+    }, "")
   }
 
   at(index: number) {
     const normalized = this.normalizeIndex(index);
-    return this.nums[normalized];
+    if (normalized >= this.digits || normalized < 0) {
+      return undefined;
+    }
+    const slotIndex = Math.floor(normalized / 2);
+    const [first, second] = this.unpack(this.nums[slotIndex]);
+    return this.isOdd(normalized) ? second : first;
   }
 
   private split(num: number | bigint): Uint8Array {
-    const arr: number[] = []
+    let arr = new Uint8Array(2);
+    let digits = 0;
     let index = 0;
+    let shift = 0 // 0 или 4;
+
     do {
-      const rem = this.remain(num, 10);
-      arr.push(Number(rem));
+      const rem = Number(this.remain(num, 10));
+      if (index >= arr.length - 1) {
+        const newArr = new Uint8Array(arr.length * 2);
+        newArr.set(arr)
+        arr = newArr;
+      }
       num = this.sub(num, rem);
       num = this.divide(num, 10);
-      index++
+
+      // упаковываем число
+      arr[index] |= rem << shift;
+      // увеличиваем индекс на 0 или 1 (4 >> 2) -> 1
+      index += shift >> 2;
+      // переключаем сдвиг
+      shift ^= 4;
+
+      digits++;
     } while (num > 0)
 
-    return new Uint8Array(arr);
+    this.digits = digits;
+    return arr.subarray(0,  Math.floor(digits / 2) + (shift >> 2))
   }
 
   private join(buffer: Uint8Array): number | bigint {
-    return buffer.reduce<number | bigint>((sum, n, i) => {
-      const carry = this.pow(10, i);
-      if (!this.isBigint(sum) && i >= 31 && this.add(sum, this.multiply(n, carry)) > Number.MAX_SAFE_INTEGER) {
+    const add = (sum: number | bigint, n: number, pow: number) => {
+      const carry = this.pow(10, pow);
+      if (!this.isBigint(sum) && this.add(sum, this.multiply(n, carry)) > Number.MAX_SAFE_INTEGER) {
         sum = BigInt(sum)
       }
       return this.add(sum, this.multiply(n, carry));
+    }
+    return buffer.reduce<number | bigint>((sum, n, i) => {
+      const [first, second] = this.unpack(n);
+      i = i * 2;
+      return add(add(sum, first, i), second, i + 1);
     }, 0)
   }
 
   /** тк разряды числа лежат в обратном порядке их нужно развернуть */
   private normalizeIndex(index: number): number {
-    const length = this.nums.length;
+    const length = this.digits;
     if (index < 0) {
       index = length + index;
     }
@@ -108,8 +147,21 @@ export class BCD {
     }
   }
 
-
   private isBigint(n: number | bigint): n is bigint {
     return typeof n === "bigint";
+  }
+
+  private isOdd(n: number) {
+    return Boolean(n & 1);
+  }
+
+  private unpack(n: number): [number, number] {
+    const first = n & FIRST_MASK;
+    const second = (n & SECOND_MASK) >>> 4;
+    return [first, second];
+  }
+
+  private isLast(arr: ArrayLike<number>, index: number): boolean {
+    return arr.length -1 === index;
   }
 }
--- packages/l3-bcd/index.ts
+++ packages/l3-bcd/index.ts
@@ -1,12 +1,21 @@
 import { BCD } from "./bcd.ts";
+import assert from "node:assert";
 
-const n = new BCD(BigInt(2**88));
+const tests = [0, 1234, BigInt(2**53 - 1)];
+// at (1, 2, -1, -2)
+const atTests = [[0, undefined, 0, undefined], [1, 2, 4, 3], [9, 0, 1, 9]]
 
-console.log(n.toBigint());
-console.log(n.toNumber());
-console.log(n.toString())
+for (let i = 0; i < tests.length; i++) {
+  const test = tests[i];
+  const bcd = new BCD(test);
+  assert.equal(bcd.toNumber(), Number(test), `Failed to convert number ${test}`);
+  assert.equal(bcd.toBigint(), BigInt(test), `Failed to convert bigint ${test}`);
+  assert.equal(bcd.toString(), String(test), `Failed to convert string ${test}`);
 
-console.log(n.at(0));
-console.log(n.at(1));
-console.log(n.at(-1));
-console.log(n.at(-2));
+  const atTest = atTests[i];
+  assert.equal(bcd.at(0), atTest[0], `Failed at (1) for ${test}`);
+  assert.equal(bcd.at(1), atTest[1], `Failed at (2) for ${test}`);
+  assert.equal(bcd.at(-1), atTest[2], `Failed at (-1) for ${test}`);
+  assert.equal(bcd.at(-2), atTest[3], `Failed at (-2) for ${test}`);
+}
+

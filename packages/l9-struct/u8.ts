a9ed915b0d9a43a5020ce1449610d231ac5a2c6e
brattik <bulki86@gmail.com>
2026-05-02 19:59:32 +0300

    добавляет воркспейс для 9 домашки и u8

---
+++ packages/l9-struct/u8.ts
@@ -0,0 +1,36 @@
+import { Struct } from "./types.ts";
+
+export class U8 extends Struct<number> {
+  private view: Uint8Array | null = null;
+
+  get(): number {
+    if (this.view == null) {
+      throw new Error(`${this.constructor.name} is not a initialized`);
+    }
+    return this.view[0];
+  }
+
+  set(value: number) {
+    if (this.view == null) {
+      throw new Error(`${this.constructor.name} is not a initialized`);
+    }
+    this.view[0] = value;
+  }
+
+  constructor() {
+    super(1, 0);
+  }
+
+
+  from(buffer: ArrayBuffer, offset: number): this {
+    this.view = new Uint8Array(buffer, offset);
+    return this;
+  }
+
+  init(buffer: ArrayBuffer, offset: number, data: number): this {
+    this.view = new Uint8Array(buffer, offset);
+    this.view[0] = data;
+
+    return this;
+  }
+}
